acc6a9b75e56a9dcfb59bb78afcc682ebf520a48
Jean <jeanmaster777@gmail.com>
2024-01-13 12:56:40 -0300

    iconfiguration

--- src/TechChallenge-FIAP_API.Application/Service/InvestimentoService.cs
+++ src/TechChallenge-FIAP_API.Application/Service/InvestimentoService.cs
@@ -1,10 +1,12 @@
-﻿using TechChallenge_FIAP_API.Application.Interface;
+﻿using Microsoft.Extensions.Configuration;
+using TechChallenge_FIAP_API.Application.Interface;
 using TechChallenge_FIAP_API.Domain.Entities;
 
 namespace TechChallenge_FIAP_API.Application.Service
 {
     public class InvestimentoService : IInvestimentoService
     {
+        private readonly IConfiguration _configuration;
         public Task<DadosAtivo> ObterDadosAtivo(string nomeAtivo)
         {
             throw new NotImplementedException();
